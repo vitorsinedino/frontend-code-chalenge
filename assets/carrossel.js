d79b560fb4823d58259b1ce05258b3917d6ae9bd
Vitor Fraifer <vitorfraifer10@gmail.com>
2024-11-14 20:00:41 -0300

    feat: implementação do slider dos produtos relacionados

---
+++ assets/carrossel.js
@@ -0,0 +1,15 @@
+console.log("TESTANDO IMPORTAÇÃO DE JS");
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var elem = document.querySelector('.carrossel');
+      if (elem) {
+        new Flickity(elem, {
+          cellAlign: 'left',
+          contain: true,
+          pageDots: false,
+          wrapAround: true
+        });
+      }
+    }, 500); //Adicionei um delay pra garantir que o slider carregue, assim como nos foi orientado via email
+  });
